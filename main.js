ec716f64d722506734b6932041f52e55453ba581
Kellen Parker <kellenrparker@gmail.com>
2021-05-25 17:28:32 -0500

    added add army button

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ function displayElements(user, comp){
 		addElement(false,i);
 	}
 
+	$('#user').append('<button class="addArmy" id="addArmyUser">Add Army</button>');
+	$('#comp').append('<button class="addArmy" id="addArmyComp">Add Army</button>');
+
 	console.log("hello");
 
 }
@@ -20,16 +23,15 @@ function displayElements(user, comp){
 function addElement(user, index){
 
 	if (user){
-		console.log("test1");
-		$('#user').append('<div class="element friendly" id="userarmy'+ index + '"></div>');
+		$('#user').append('<div class="element friendly" id="userarmy' + index + '"></div>');
 		$('#userarmy' + index).append('<h3 class="elehead">1st Army</h3>');
 		$('#userarmy' + index).append('<p class="paragraph">Infantry: 00000 Cavalry: 00000 Artillery: 00000</p>');
-		$('#userarmy' + index).append('<button class="button">Edit</button>');
+		$('#userarmy' + index).append('<button class="armybut" id="userbut' + index + '">Edit</button>');
 	}else{
 		$('#comp').append('<div class="element enemy" id="comparmy'+ index + '"></div>');
 		$('#comparmy' + index).append('<h3 class="elehead">1st Army</h3>');
 		$('#comparmy' + index).append('<p class="paragraph">Infantry: 00000 Cavalry: 00000 Artillery: 00000</p>');
-		$('#comparmy' + index).append('<button class="button">Edit</button>');
+		$('#comparmy' + index).append('<button class="armybut" id="compbut' + index + '">Edit</button>');
 	}
 
 }
