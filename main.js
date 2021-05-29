88dc29a4f4991c3d56fd8370b5ff382f1aadd394
Kellen Parker <kellenrparker@gmail.com>
2021-05-29 10:57:54 -0500

    added functionality to add army button

--- main.js
+++ main.js
@@ -1,41 +1,53 @@
 
-
-var userArmies = 1;
-var compArmies = 1;
-
-
-function displayElements(user, comp){
-
-	for(i = 0; i < user; i++){
-		addElement(true,i);
-	}
-	for(i = 0; i < comp; i++){
-		addElement(false,i);
-	}
-
-	$('#user').append('<button class="addArmy" id="addArmyUser">Add Army</button>');
-	$('#comp').append('<button class="addArmy" id="addArmyComp">Add Army</button>');
-
-	console.log("hello");
-
-}
-
-function addElement(user, index){
-
-	if (user){
-		$('#user').append('<div class="element friendly" id="userarmy' + index + '"></div>');
-		$('#userarmy' + index).append('<h3 class="elehead">1st Army</h3>');
-		$('#userarmy' + index).append('<p class="paragraph">Infantry: 00000 Cavalry: 00000 Artillery: 00000</p>');
-		$('#userarmy' + index).append('<button class="armybut" id="userbut' + index + '">Edit</button>');
-	}else{
-		$('#comp').append('<div class="element enemy" id="comparmy'+ index + '"></div>');
-		$('#comparmy' + index).append('<h3 class="elehead">1st Army</h3>');
-		$('#comparmy' + index).append('<p class="paragraph">Infantry: 00000 Cavalry: 00000 Artillery: 00000</p>');
-		$('#comparmy' + index).append('<button class="armybut" id="compbut' + index + '">Edit</button>');
-	}
-
-}
+var userArmies = 0;
+var compArmies = 0;
 
 $(document).ready(function(){
-	displayElements(userArmies, compArmies);
+	addArmy(true);
+	addArmy(false)
 });
+
+function addArmy(user){
+
+	if(user){
+		if(userArmies >= 9){
+			return;	
+		} 
+		$('#addArmyUser').remove();
+		userArmies++;
+		$('#user').append('<div class="element friendly" id="userArmy' + userArmies + '"></div>');
+		$('#userArmy' + userArmies).append('<h3 class="elehead">' + userArmies + genArmyNumber(userArmies) + ' Army</h3>');
+		$('#userArmy' + userArmies).append('<p class="paragraph">Infantry: 00000 Cavalry: 00000 Artillery: 00000</p>');
+		$('#userArmy' + userArmies).append('<button class="armyBut" id="userBut' + userArmies + '">Edit</button>');
+		$('#user').append('<button class="addArmy" id="addArmyUser">Add Army</button>');
+		$('#addArmyUser').on("click", function(){
+			addArmy(true);
+		});
+	}
+	else {
+		if(compArmies >= 9){
+			return;	
+		} 
+		$('#addArmyComp').remove();
+		compArmies++;
+		$('#comp').append('<div class="element enemy" id="compArmy'+ compArmies + '"></div>');
+		$('#compArmy' + compArmies).append('<h3 class="elehead">' + compArmies + genArmyNumber(compArmies) + ' Army</h3>');
+		$('#compArmy' + compArmies).append('<p class="paragraph">Infantry: 00000 Cavalry: 00000 Artillery: 00000</p>');
+		$('#compArmy' + compArmies).append('<button class="armyBut" id="compBut' + compArmies + '">Edit</button>');
+		$('#comp').append('<button class="addArmy" id="addArmyComp">Add Army</button>');		
+		$('#addArmyComp').on("click", function(){
+			addArmy(false);
+		});
+	}
+
+}
+
+//Will only need to check up to army limit (10)
+function genArmyNumber(num){
+
+	if(!(num === 1 || num === 3 || num === 2)) return 'th';
+	else if (num === 2) return 'nd';
+	else if (num === 3) return 'rd';
+	else return 'st';
+
+}
